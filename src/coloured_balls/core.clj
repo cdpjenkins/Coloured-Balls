f7a1ef73abb098d636e96bbbef6c9ab50acd151d
cdpjenkins <cdpjenkins@gmail.com>
2011-04-30 22:10:09 +0100

    Balls now move and bounce off walls

--- src/coloured_balls/core.clj
+++ src/coloured_balls/core.clj
@@ -6,30 +6,39 @@
 (def WIDTH 400)
 (def HEIGHT 400)
 
+(defn my-rand [min max]
+  (let [range (- max min)]
+    (+ (rand range) min)))
+
 ;; here's a function which will be called by Processing's (PApplet)
 ;; draw method every frame. Place your code here. If you eval it
 ;; interactively, you can redefine it while the applet is running and
 ;; see effects immediately
 
 (defn draw-ball [ball]
-;  (stroke (255 255 255))
-  (println "hoss" ball)
+  (stroke 0 0 0)
   (fill (:red ball) (:green ball) (:blue ball))
-  (ellipse (:x ball) (:y ball) (:radius ball) (:radius ball)))
+  (let [x (:x ball)
+        y (:y ball)
+        diameter (* (:radius ball) 2)]
+    (ellipse x y diameter diameter)    
+    )
+  )
 
 (defn make-ball []
-  {:x (rand-int 400)
-   :y (rand-int 400)
-   :vx (- (rand-int 10) 5)
-   :vy (- (rand-int 10) 5)
-   :red (rand-int 256)
-   :blue (rand-int 256)
-   :green (rand-int 256)
-   :radius (+ 1 (rand-int 70))})
+  (let [radius (rand 50)]
+    {:x (my-rand radius (- WIDTH radius))
+     :y (my-rand radius (- HEIGHT radius))
+     :vx (- (rand 10) 5)
+     :vy (- (rand 10) 5)
+     :red (rand-int 256)
+     :blue (rand-int 256)
+     :green (rand-int 256)
+     :radius radius}))
 
 (def *balls*
   (atom 
-   (for [_ (range 10)]
+   (for [_ (range 20)]
      (make-ball))))
 
 (defn move-balls [balls]
@@ -40,10 +49,11 @@
   (for [ball balls]
     (let [nx (+ (:x ball) (:vx ball))
           ny (+ (:y ball) (:vy ball))
-          nvx (if (or (< nx 0) (> nx WIDTH))
+          radius (:radius ball)
+          nvx (if (or (< (- nx radius) 0) (> (+ nx radius) WIDTH))
                 (- (:vx ball))
                 (:vx ball))
-          nvy (if (or (< ny 0) (> ny HEIGHT))
+          nvy (if (or (< (- ny radius) 0) (> (+ ny radius) HEIGHT))
                 (- (:vy ball))
                 (:vy ball))]
                 
@@ -53,20 +63,28 @@
                   :vx nvx
                   :vy nvy))))
 
+;;; 
+(defn cls []
+  (smooth)
+  (no-stroke)
+  (fill 226)
+  (rect 0 0 WIDTH HEIGHT))
+
 (defn draw
   "Example usage of with-translation and with-rotation."
   []
   (swap! *balls* move-balls)
+
+  (cls)
   (doseq [ball @*balls*]
     (draw-ball ball))
-  ;;	(draw-ball (make-ball))
-  )
+  (stroke 255 0 0))
+
+
 
 (defn setup []
   "Runs once."
-  (smooth)
-;  (no-stroke)
-  (fill 226)
+  (cls)
   (framerate 10))
 
 ;; Now we just need to define an applet:
